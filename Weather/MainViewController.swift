e6d854c3e463893cf7fa030ab0c375b478c7587e
Ildar <gataullin.i@gmail.com>
2019-10-16 13:29:28 +0300

    Initial

---
+++ Weather/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Macbook on 16.10.2019.
+//  Copyright © 2019 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let names = ["Ine","tow","dhet","Fiebv","Sixx"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return names.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+   
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
